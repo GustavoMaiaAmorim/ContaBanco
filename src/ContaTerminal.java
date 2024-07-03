d2fc2cf1cada70141ff285ffd76edfb0b28d166a
Gustavo Maia <maiag373@gmail.com>
2024-07-02 21:57:03 -0300

    ProjetoJava

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,38 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+
+public class ContaTerminal {  
+    public static void main(String[] args) {
+        
+        //Criando objeto scanner para recebr as informações no terminal
+        Scanner sc = new Scanner(System.in);
+
+        //declarando variáveis/atributos
+        String agencia;
+        String nomeCliente;
+        int numero;
+        float saldo;
+       
+
+        //Enviando e recebendo dados do terminal
+        System.out.println("Por favor, digite o numero da agencia:");
+        agencia = sc.nextLine();
+
+        System.out.println("Por favor, digite o nome:");
+        nomeCliente = sc.nextLine();
+        
+        System.out.println("Por favor, digite o numero:");
+        numero = sc.nextInt();
+       
+        System.out.println("Por favor, digite o saldo:");
+        saldo = sc.nextFloat();
+
+        sc.close();
+        
+        //Enviando mensagem final ao usuário
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta: " + numero + " e seu saldo R$: " + saldo  + " já está disponível para saque.");
+ 
+        
+       
     }
 }
